e42544719a674e4074ec8095b583770ea5b97e10
Lennyyyyyyyyyy <lennya903@gmail.com>
2024-11-05 14:11:50 +0100

    test

--- mission2/src/mission2/FormulaireCommande.java
+++ mission2/src/mission2/FormulaireCommande.java
@@ -1,18 +1,17 @@
 package mission2;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
 import java.awt.Font;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class FormulaireCommande extends JFrame {
 
